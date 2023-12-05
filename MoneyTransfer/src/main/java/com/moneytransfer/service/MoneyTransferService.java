48bf178017bd750df788606bd668d0a77dc01cb8
Goran Marić <45515666+GocaMaric@users.noreply.github.com>
2023-12-05 20:48:54 +0100

    Update MoneyTransferService.java

--- MoneyTransfer/src/main/java/com/moneytransfer/service/MoneyTransferService.java
+++ MoneyTransfer/src/main/java/com/moneytransfer/service/MoneyTransferService.java
@@ -6,34 +6,48 @@ import com.moneytransfer.model.MoneyTransfer;
 import com.moneytransfer.repository.MoneyTransferRepository;
 import com.moneytransfer.request.JsonRequest;
 
+import java.util.List;
+
 @Service
 public class MoneyTransferService {
-  
-  @Autowired
-  private SendingService sendingService;
-  
-  @Autowired
-  private MoneyTransferRepository moneyTransferRepository;
-  
-  /**
-   * Save money
-   * @param jsonRequest
-   * @return
-   */
-  public MoneyTransfer sendMoney(JsonRequest jsonRequest) {
-    MoneyTransfer moneyTransfer = new MoneyTransfer();
-    moneyTransfer.setAmount(jsonRequest.getAmount());
-    moneyTransfer.setCurrency(jsonRequest.getCurrency());
-    moneyTransferRepository.save(moneyTransfer);
-    
-    //Send Message 
-    sendingService.sendMessage(moneyTransfer);
-    
-    return moneyTransfer;
-  }
 
-  public Object findAll() {
-    return moneyTransferRepository.findAll();
-  }
-  
+    @Autowired
+    private SendingService sendingService;
+
+    @Autowired
+    private MoneyTransferRepository moneyTransferRepository;
+
+    /**
+     * Save money transfer details
+     *
+     * @param jsonRequest
+     * @return
+     */
+    public MoneyTransfer saveMoneyTransfer(JsonRequest jsonRequest) {
+        MoneyTransfer moneyTransfer = new MoneyTransfer();
+        moneyTransfer.setAmount(jsonRequest.getAmount());
+        moneyTransfer.setCurrency(jsonRequest.getCurrency());
+
+        try {
+            moneyTransferRepository.save(moneyTransfer);
+
+            // Send Message
+            sendingService.sendMessage(moneyTransfer);
+
+            return moneyTransfer;
+        } catch (Exception e) {
+            // Handle the exception (log, throw, or return an error response)
+            e.printStackTrace(); // Consider using a logger instead of printing to the console
+            return null;
+        }
+    }
+
+    /**
+     * Get all money transfers
+     *
+     * @return
+     */
+    public List<MoneyTransfer> findAll() {
+        return moneyTransferRepository.findAll();
+    }
 }
