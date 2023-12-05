1ea75eeb723fc75b34ae5f66c2da1c99447ecc78
Goran Marić <45515666+GocaMaric@users.noreply.github.com>
2023-12-05 20:47:54 +0100

    Update JsonRequest.java

--- MoneyTransfer/src/main/java/com/moneytransfer/request/JsonRequest.java
+++ MoneyTransfer/src/main/java/com/moneytransfer/request/JsonRequest.java
@@ -1,24 +1,31 @@
 package com.moneytransfer.request;
 
+import java.math.BigDecimal;
+
 public class JsonRequest {
 
-  private Double amount;
-  private String currency;
+    private BigDecimal amount;
+    private String currency;
 
-  public Double getAmount() {
-    return amount;
-  }
+    public BigDecimal getAmount() {
+        return amount;
+    }
 
-  public void setAmount(Double amount) {
-    this.amount = amount;
-  }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 
-  public String getCurrency() {
-    return currency;
-  }
+    public String getCurrency() {
+        return currency;
+    }
 
-  public void setCurrency(String currency) {
-    this.currency = currency;
-  }
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
 
+    // Add validation methods as needed
+
+    public boolean isValid() {
+        return amount != null && amount.compareTo(BigDecimal.ZERO) >= 0 && currency != null && !currency.isEmpty();
+    }
 }
