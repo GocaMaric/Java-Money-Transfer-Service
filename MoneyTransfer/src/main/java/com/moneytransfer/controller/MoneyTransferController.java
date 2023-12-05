99c2c89345e8c93a2f140171e39e86b5704568d9
Goran Marić <45515666+GocaMaric@users.noreply.github.com>
2023-12-05 20:45:54 +0100

    Update MoneyTransferController.java

--- MoneyTransfer/src/main/java/com/moneytransfer/controller/MoneyTransferController.java
+++ MoneyTransfer/src/main/java/com/moneytransfer/controller/MoneyTransferController.java
@@ -1,49 +1,46 @@
-package com.moneytransfer.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import com.moneytransfer.model.MoneyTransfer;
-import com.moneytransfer.request.JsonRequest;
-import com.moneytransfer.service.MoneyTransferService;
-
 @RestController
 public class MoneyTransferController {
-  
-  @Autowired
-  private MoneyTransferService moneyTransferService;
-  
-  /**
-   * Send money
-   * @param jsonRequest
-   * @return
-   */
-  @PostMapping("/api/send-money")
-  public ResponseEntity<Object> createUser(@RequestBody JsonRequest jsonRequest) {
-    if(!jsonRequest.getCurrency().equals("EUR")) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You entered wrong currncy, We only accept EUR");
+
+    @Autowired
+    private MoneyTransferService moneyTransferService;
+
+    /**
+     * Send money
+     *
+     * @param jsonRequest
+     * @return
+     */
+    @PostMapping("/api/send-money")
+    public ResponseEntity<Object> sendMoney(@RequestBody JsonRequest jsonRequest) {
+        ResponseEntity<Object> validationResponse = validateSendMoneyRequest(jsonRequest);
+        if (validationResponse != null) {
+            return validationResponse;
+        }
+
+        MoneyTransfer moneyTransfer = moneyTransferService.sendMoney(jsonRequest);
+        return ResponseEntity.ok(moneyTransfer);
     }
-    if(jsonRequest.getAmount() <= -100000000) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount property must not be lower than -100000000 EUR");
+
+    private ResponseEntity<Object> validateSendMoneyRequest(JsonRequest jsonRequest) {
+        if (!"EUR".equals(jsonRequest.getCurrency())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("We only accept EUR currency");
+        }
+        if (jsonRequest.getAmount() < -100000000) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount must not be lower than -100000000 EUR");
+        }
+        if (jsonRequest.getAmount() > 100000000) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount must not be larger than 100000000 EUR");
+        }
+        return null;
     }
-    if(jsonRequest.getAmount() >= 100000000) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount property must not be larger than +100000000 EUR");
+
+    /**
+     * Get all money transactions
+     *
+     * @return
+     */
+    @GetMapping("/api/money-transactions")
+    public ResponseEntity<Object> getAllMoneyTransactions() {
+        return ResponseEntity.ok(moneyTransferService.findAll());
     }
-    MoneyTransfer moneyTransfer = moneyTransferService.sendMoney(jsonRequest);
-    return ResponseEntity.ok(moneyTransfer);
-  }
-  
-  /**
-   * Send money
-   * @param jsonRequest
-   * @return
-   */
-  @GetMapping("/api/money-transactions")
-  public ResponseEntity<Object> getAllTransactions() {
-    return ResponseEntity.ok(moneyTransferService.findAll());
-  }
 }
